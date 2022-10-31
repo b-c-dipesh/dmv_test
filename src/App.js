115d779f55146051fdb57fde093e70191f1b8a40
bc-dipesh <bcdipeshwork@gmail.com>
2022-10-31 13:08:23 -0400

    Add logic to check user ans and track score

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import questions from './questions.json';
 import './App.css';
 
 const App = () => {
 	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [isCorrect, setIsCorrect] = useState(false);
+	const [score, setScore] = useState(0);
+	const feedbackRef = useRef();
+
+	const checkAnswer = (value) => {
+		if (value === questions[currentQuestion].correctAnswer) {
+			feedbackRef.current.classList.remove('d-none');
+			setScore(score + 1);
+			setIsCorrect(true);
+		} else {
+			feedbackRef.current.classList.remove('d-none');
+			setIsCorrect(false);
+		}
+	};
+
+	const displayNextQuestion = () => {
+		feedbackRef.current.classList.add('d-none');
+		setCurrentQuestion(currentQuestion + 1);
+	};
 
 	return (
 		<div className="container-fluid min-vh-100 bg-light">
 			<main className="d-flex flex-column justify-content-center align-items-start min-vh-100 py-5">
+				<span className="fw-bold fs-5">{score}</span>
+				<span className="badge bg-info">
+					{isCorrect ? 'Correct' : 'Wrong'}
+				</span>
 				<section className="qna-section rounded-4 min-vh-50">
 					<h1 className="fw-bold question">{`${
 						currentQuestion + 1
 					}. ${questions[currentQuestion].question}`}</h1>
-					<article className="mt-5 feedback shadow rounded alert alert-light">
+					<article
+						ref={feedbackRef}
+						className="mt-5 feedback shadow rounded alert alert-secondary"
+					>
 						<p>Feedback</p>
 						<p>{questions[currentQuestion].feedback}</p>
 					</article>
@@ -23,6 +49,7 @@ const App = () => {
 									<li
 										key={index * 3}
 										className="shadow rounded my-2 list-group-item text-capitalize"
+										onClick={(e) => checkAnswer(ans.value)}
 									>
 										{ans.text}
 									</li>
@@ -34,7 +61,7 @@ const App = () => {
 				<button
 					type="button"
 					className="mt-5 btn btn-secondary shadow rounded-4"
-					onClick={(e) => setCurrentQuestion(currentQuestion + 1)}
+					onClick={displayNextQuestion}
 				>
 					Next Question
 				</button>
