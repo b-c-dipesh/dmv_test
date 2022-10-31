4a02b1dca2f1c4acb4bd3ce380594756d18bdb60
bc-dipesh <bcdipeshwork@gmail.com>
2022-10-31 12:43:32 -0400

    Make app responsive

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { useState } from 'react';
 import questions from './questions.json';
+import './App.css';
 
 const App = () => {
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 
 	return (
-		<div className="container-fluid">
-			<main>
-				<section className="shadow rounded-4 p-5 mt-5 min-vh-50">
-					<h2>{`${currentQuestion + 1}. ${
-						questions[currentQuestion].question
-					}`}</h2>
-					<ol className="my-5 list-group list-group-numbered list-group-flush">
-						{questions[currentQuestion].answers.map((ans) => {
-							return (
-								<li className="list-group-item text-capitalize">
-									{ans.text}
-								</li>
-							);
-						})}
-					</ol>
-					<article className="alert alert-light">
+		<div className="container-fluid min-vh-100 bg-light">
+			<main className="d-flex flex-column justify-content-center align-items-start min-vh-100 py-5">
+				<section className="qna-section rounded-4 min-vh-50">
+					<h1 className="fw-bold question">{`${
+						currentQuestion + 1
+					}. ${questions[currentQuestion].question}`}</h1>
+					<article className="mt-5 feedback shadow rounded alert alert-light">
+						<p>Feedback</p>
 						<p>{questions[currentQuestion].feedback}</p>
 					</article>
+					<ol className="my-5 list-group list-group-numbered list-group-flush">
+						{questions[currentQuestion].answers.map(
+							(ans, index) => {
+								return (
+									<li
+										key={index * 3}
+										className="shadow rounded my-2 list-group-item text-capitalize"
+									>
+										{ans.text}
+									</li>
+								);
+							}
+						)}
+					</ol>
 				</section>
 				<button
 					type="button"
