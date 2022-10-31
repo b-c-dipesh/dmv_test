a278ab1fbb6fd88f3faa2f553858b1985a6f4c04
bc-dipesh <bcdipeshwork@gmail.com>
2022-10-30 22:38:16 -0400

    Create app layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import questions from './questions.json';
 
 const App = () => {
-	return <div>App</div>;
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+
+	return (
+		<div className="container-fluid">
+			<main>
+				<section className="shadow rounded-4 p-5 mt-5 min-vh-50">
+					<h2>{`${currentQuestion + 1}. ${
+						questions[currentQuestion].question
+					}`}</h2>
+					<ol className="my-5 list-group list-group-numbered list-group-flush">
+						{questions[currentQuestion].answers.map((ans) => {
+							return (
+								<li className="list-group-item text-capitalize">
+									{ans.text}
+								</li>
+							);
+						})}
+					</ol>
+					<article className="alert alert-light">
+						<p>{questions[currentQuestion].feedback}</p>
+					</article>
+				</section>
+				<button
+					type="button"
+					className="mt-5 btn btn-secondary shadow rounded-4"
+					onClick={(e) => setCurrentQuestion(currentQuestion + 1)}
+				>
+					Next Question
+				</button>
+			</main>
+		</div>
+	);
 };
 
 export default App;
