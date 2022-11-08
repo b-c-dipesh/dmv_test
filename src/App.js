fcc543038c6faa41eda3ab5aa7ad1a8e1539de3a
bc-dipesh <bcdipeshwork@gmail.com>
2022-11-08 08:53:54 -0500

    Add Score counter and logic to check user answer

--- src/App.js
+++ src/App.js
@@ -6,40 +6,63 @@ const App = () => {
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [isCorrect, setIsCorrect] = useState(false);
 	const [score, setScore] = useState(0);
-	const feedbackRef = useRef();
+	const answerRef = useRef();
+	const rightWrongInfoRef = useRef();
+
+	const viewAnswer = () => {
+		answerRef.current.classList.toggle('d-none');
+	};
 
 	const checkAnswer = (value) => {
 		if (value === questions[currentQuestion].correctAnswer) {
-			feedbackRef.current.classList.remove('d-none');
 			setScore(score + 1);
 			setIsCorrect(true);
 		} else {
-			feedbackRef.current.classList.remove('d-none');
 			setIsCorrect(false);
 		}
+		rightWrongInfoRef.current.classList.remove('d-none');
 	};
 
 	const displayNextQuestion = () => {
-		feedbackRef.current.classList.add('d-none');
+		answerRef.current.classList.add('d-none');
+		rightWrongInfoRef.current.classList.add('d-none');
 		setCurrentQuestion(currentQuestion + 1);
 	};
 
 	return (
 		<div className="container-fluid min-vh-100 bg-light">
 			<main className="d-flex flex-column justify-content-center align-items-start min-vh-100 py-5">
-				<span className="fw-bold fs-5">{score}</span>
-				<span className="badge bg-info">
-					{isCorrect ? 'Correct' : 'Wrong'}
-				</span>
+				<section ref={rightWrongInfoRef} className="right-wrong-info">
+					{isCorrect && (
+						<p className="fw-bold fs-5 d-flex align-items-center">
+							{score}{' '}
+							<span className="ms-1 badge bg-info shadow">
+								Correct
+							</span>
+						</p>
+					)}
+					{isCorrect || (
+						<p className="fw-bold fs-5 badge bg-danger shadow">
+							Incorrect
+						</p>
+					)}
+				</section>
 				<section className="qna-section rounded-4 min-vh-50">
 					<h1 className="fw-bold question">{`${
 						currentQuestion + 1
 					}. ${questions[currentQuestion].question}`}</h1>
-					<article
-						ref={feedbackRef}
-						className="mt-5 feedback shadow rounded alert alert-secondary"
+					<button
+						type="button"
+						className="btn btn-secondary shadow rounded-4"
+						onClick={viewAnswer}
 					>
-						<p>Feedback</p>
+						View Answer
+					</button>
+					<article
+						ref={answerRef}
+						className="mt-5 feedback shadow rounded alert alert-secondary d-none answer"
+					>
+						<p>Answer</p>
 						<p>{questions[currentQuestion].feedback}</p>
 					</article>
 					<ol className="my-5 list-group list-group-numbered list-group-flush">
